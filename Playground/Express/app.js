d8c09ecb24e78d8da881f98f1ddb398d41f43b80
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2023-09-17 20:14:47 +0530

    added all files

---
+++ Playground/Express/app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const { isUtf8 } = require("node:buffer");
+const exp = require("node:constants");
+const fs = require("node:fs");
+const morgan = require("morgan");
+
+const tours = JSON.parse(fs.readFileSync("./dev-data/tours.json"));
+const PORT = 3000;
+
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const tourID = req.params.id;
+  if (tourID < tours.length) {
+    res.status(200).json({
+      status: "success",
+      data: tours[tourID],
+    });
+  } else {
+    res.status(404).json({
+      status: "failure",
+      message: `Tour with id: ${tourID} does not exist`,
+    });
+  }
+};
+
+const createTour = (req, res) => {
+  const id = tours.length;
+  const newTour = { id: id, ...req.body };
+  tours.push(newTour);
+  fs.writeFile("./dev-data/tours.json", JSON.stringify(tours), (err) => {
+    if (!err) {
+      res.status(201).json({
+        status: "success",
+        data: {
+          newTour,
+        },
+      });
+    } else {
+      res.status(400).json("err");
+    }
+  });
+};
+
+app.use((req, res, next) => {
+  console.log("hello from middleware");
+  res.requestTime = new Date().toLocaleString();
+  next();
+});
+
+app.route("/api/v1/tours").get(getAllTours).post(createTour);
+app.route("/api/v1/tours/:id").get(getTour);
+
+app.listen(PORT, () => {
+  console.log(`App running on PORT: ${PORT}`);
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+Welcome to the repository of mini backend projects developed using Node.js and Express.js. This collection of projects serves as a hands-on learning experience for backend development in the Node.js ecosystem.
